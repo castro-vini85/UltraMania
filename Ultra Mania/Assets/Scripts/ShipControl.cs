de7438e2f2a43f4c74ec188103eadb347794783b
castro-vini85 <castro.vini85@gmail.com>
2016-10-06 15:49:29 -0700

    Creation of the project
    Creation of the project
    Some tests for the levels

---
+++ Ultra Mania/Assets/Scripts/ShipControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipControl : MonoBehaviour
+{
+
+    public int force = 3000;
+    public GameObject shotObj;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(-force, 0));
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(force, 0));
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var shipPosition = GetComponent<Rigidbody2D>().position;
+
+            shipPosition.y += 0.5f;
+
+            Instantiate(shotObj, shipPosition, Quaternion.identity);
+        }
+    }
+}
---
+++ Ultra Mania/Assets/Scripts/ShotControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotControl : MonoBehaviour
+{
+
+    public int force = 1000;
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(0, force));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
